f08a0e4d2d3bcc69b315dced8e14566078175761
李鼎权, Dingquan Li <dingquanli@pku.edu.cn>
2021-09-29 12:38:10 +0800

    Unneeded

--- encode/mpeg-pcc-tmc13/clear_octree-liftt-ctc-lossy-geom-lossy-attrs_bin_and_log.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-del "/mnt/d/Downloads/PCL/Datasets/pointcloud/PointXR/PointXR-dataset-15/octree-liftt-ctc-lossy-geom-lossy-attrs/*.log" "/mnt/d/Downloads/PCL/Datasets/pointcloud/PointXR/PointXR-dataset-15/octree-liftt-ctc-lossy-geom-lossy-attrs/*.bin" 
